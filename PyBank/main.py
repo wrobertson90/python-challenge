5c96b1ba135207373bd17d9ad80d10f3829f6c2d
will <wwrobertson1225@gmail.com>
2025-01-13 16:56:07 -0700

    ..

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,5 @@
 import csv
 import os
-os.makedirs("analysis", exist_ok= True)
 file_to_load = os.path.join("PyBank/Resources", '../Resources/budget_data.csv' )  # Input file path
 file_to_output = os.path.join("../analysis", '../budget_analysis.txt')  # Output file path
 #variables to track financial data
@@ -9,25 +8,28 @@ total_net = 0
 previous_net=0
 changes=[]
 greatest_increase=["", 0]
-greatest_decrease=["", 99999999]
-
+greatest_decrease=["", 0]
+#skip header
 with open(file_to_load) as budget_data:
     reader = csv.reader(budget_data)
     header = next(reader)
     first_row=next(reader)
+    #track months and net
     total_months += 1
     total_net += int(first_row[1])
     previous_net = int(first_row[1])
-
+#set date and net locations
     for row in reader:
         date = row[0]
         net = int(row[1])
+        #track months changes
         total_months += 1
         total_net += net
         net_change = net-previous_net
         previous_net=net
+        #attach changes
         changes.append(net_change)
-
+#track net largest changes both increase and decrease
         if net_change>greatest_increase[1]:
             greatest_increase[0]=date
             greatest_increase[1]=net_change
@@ -35,8 +37,9 @@ with open(file_to_load) as budget_data:
         if net_change<greatest_decrease[1]:
             greatest_decrease[0]=date
             greatest_decrease[1]=net_change
-
+#calculate average change
 average_change= sum(changes)/ len(changes)
+#show print and write the data out
 output= (f"Financial Analysis"
          f"--------------\n"
          f"Total Months: {total_months}\n"
