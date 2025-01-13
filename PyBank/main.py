a267b6e26530f13a3d1d1974b8efc84ebd6bf413
will <wwrobertson1225@gmail.com>
2025-01-13 16:19:38 -0700

    cleanup

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import csv
 import os
 os.makedirs("analysis", exist_ok= True)
 file_to_load = os.path.join("PyBank/Resources", '../Resources/budget_data.csv' )  # Input file path
-file_to_output = os.path.join("analysis", 'budget_analysis.txt')  # Output file path
+file_to_output = os.path.join("../analysis", '../budget_analysis.txt')  # Output file path
 #variables to track financial data
 total_months = 0
 total_net = 0
@@ -40,10 +40,10 @@ average_change= sum(changes)/ len(changes)
 output= (f"Financial Analysis"
          f"--------------\n"
          f"Total Months: {total_months}\n"
-         f"Total: ${total_net}\n"
-         f"Average Change: ${average_change:.2f}\n"
-         f"Greatest Increase In Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
-         f"Greatest Decrease In Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n"
+         f"Total: ${total_net:,}\n"
+         f"Average Change: ${average_change:,}\n"
+         f"Greatest Increase In Profits: {greatest_increase[0]} (${greatest_increase[1]:,})\n"
+         f"Greatest Decrease In Profits: {greatest_decrease[0]} (${greatest_decrease[1]:,})\n"
          )
 print(output)
 with open(file_to_output, "w") as txt_file:
