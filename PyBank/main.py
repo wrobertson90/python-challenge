046ceea777926e4b4dcceb96e9f549ed83e617fd
will <wwrobertson1225@gmail.com>
2025-01-13 18:34:32 -0700

    updated main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import csv
 import os
-file_to_load = os.path.join("PyBank/Resources", '../Resources/budget_data.csv' )  # Input file path
-file_to_output = os.path.join("../analysis", '../budget_analysis.txt')  # Output file path
+file_to_load = os.path.join("Resources","budget_data.csv")  # Input file path
+file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
 #variables to track financial data
 total_months = 0
 total_net = 0
